2ef9404e37c2b2611b45ee22f00250b426fddae1
Xiang990293 <60068728+Xiang990293@users.noreply.github.com>
2023-02-22 16:00:12 +0800

    Update a694_eat_whole_world_II.cpp

--- a694_eat_whole_world_II.cpp
+++ a694_eat_whole_world_II.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n,m;
-    while(cin>>n>>m){
-        int vertical_array[n+1][n];
-        int horizontal_array[n][n+1];
-        vertical_array[0][0]=0;
-        horizontal_array[0][0]=0;
-        for(int k=1;k<=n;k++){
-            horizontal_array[0][k]=horizontal_array[n-1][k];
-            for(int l=1;l<=n;l++){
-                int nnfood;
-                cin >> nnfood;
-                horizontal_array[l][k]=nnfood+horizontal_array[l-1][k];
-            }
-        }
-        for(int k=0;k<m;k++){
-			int i,j;
-			cin>>i>>j;
-			cout<<""<<endl;
-        }
-    }
+int main(){
+	int m=0,n=0;
+	while(cin>>n>>m){
+		int food[n][n]={0};
+		int v_s[n][n]={0}; //vertical sum
+		int h_s[n][n]={0}; //horizontal sum
+		//input and calculate horison sum in each raw
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				cin >> food[i][j];
+				if(j>0){
+					h_s[i][j] = food[i][j] + h_s[i][j-1];
+				}else{
+					h_s[i][j] = food[i][j];
+				}
+			}
+		}
+		//calculate ractangle sum in all raws before current one
+		for(int i=1; i<n; i++){
+			for(int j=n; j>0; j--){
+				h_s=[i][j]=h_s[i-1][j]+food[i][j]+h_s[i][j-1]
+			}
+		}
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				if(j>0){
+					v_s[j][i] = food[j][i] + v_s[j-1][i];
+				}else{
+					v_s[j][i] = food[j][i];
+				}
+			}
+		}
+		
+//		for(int i=0; i<n; i++){
+//			for(int j=0; j<n; j++){
+//				cout << v_s[i][j] << "";
+//			}
+//			cout << endl;
+//		}
+		cout << endl;
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				cout << h_s[i][j] << "";
+			}
+			cout << endl;
+		}
+	}
 }
