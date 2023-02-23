144367515bd55352feef8dc5bf631541aae84dc5
Xiang990293 <990293kwi@gmail.com>
2023-02-24 03:05:41 +0800

    update

--- a694_eat_whole_world_II.cpp
+++ a694_eat_whole_world_II.cpp
@@ -5,47 +5,38 @@ int main(){
 	int m=0,n=0;
 	while(cin>>n>>m){
 		int food[n][n]={0};
-		int v_s[n][n]={0}; //vertical sum
-		int h_s[n][n]={0}; //horizontal sum
+		int s[n][n]={0}; //rac_sum
 		//input and calculate horison sum in each raw
 		for(int i=0; i<n; i++){
 			for(int j=0; j<n; j++){
 				cin >> food[i][j];
 				if(j>0){
-					h_s[i][j] = food[i][j] + h_s[i][j-1];
+					s[i][j] = food[i][j] + s[i][j-1];
 				}else{
-					h_s[i][j] = food[i][j];
-				}
-			}
-		}
-		//calculate ractangle sum in all raws before current one
-		for(int i=1; i<n; i++){
-			for(int j=n; j>0; j--){
-				h_s[i][j]=h_s[i-1][j]+food[i][j]+h_s[i][j-1];
-			}
-		}
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				if(j>0){
-					v_s[j][i] = food[j][i] + v_s[j-1][i];
-				}else{
-					v_s[j][i] = food[j][i];
+					s[i][j] = food[i][j];
 				}
 			}
 		}
 		
-//		for(int i=0; i<n; i++){
-//			for(int j=0; j<n; j++){
-//				cout << v_s[i][j] << "";
-//			}
-//			cout << endl;
-//		}
-		cout << endl;
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				cout << h_s[i][j] << "";
+		for(int i=1; i<n; i++){
+			for(int j=n-1; j>=0; j--){
+				if(j>0) s[i][j]=s[i-1][j]+food[i][j]+s[i][j-1];
+				else s[i][j]=s[i-1][j]+food[i][j];
 			}
-			cout << endl;
 		}
+		
+		int x1=0,y1=0,x2=0,y2=0;
+		int x_yes = 1, y_yes = 1;
+		for(int i=0; i<m; i++){
+			cin >> x1 >> y1 >> x2 >> y2;
+			x_yes = 1, y_yes = 1;
+			x1--;
+			x2--;
+			y1--;
+			y2--;
+			if(x1==0) x_yes = 0;
+			if(y1==0) y_yes = 0;
+			cout << s[x2][y2] - x_yes*s[x1-1][y2] - y_yes*s[x2][y1-1] + x_yes*y_yes*s[x1-1][y1-1] << endl;
+		}		
 	}
 }
