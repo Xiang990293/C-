1a811dd926c77ea28c8c0ab3b95ae9eee5fc7997
Xiang990293 <60068728+Xiang990293@users.noreply.github.com>
2023-03-01 15:48:37 +0800

    final change - completed and passed

--- a121_prime_counter.cpp
+++ a121_prime_counter.cpp
@@ -2,27 +2,38 @@
 #include<math.h>
 using namespace std;
 
-const long long Largest = 100;
-const long long large_prime = 5761455;
-long long count[large_prime] = {0};
-
-long long dp[100]={0};
-long long fact(int a){
-	dp[1] = 1;
-	if(dp[a]!=0) return dp[a];
-	else for(int i=2; i<a; i++) dp[a] = fact(a-1) * a;
-	return dp[a];
+int check(int a, int b){
+	if(a==b){
+		if(a==1) return 0;
+		else {
+			for(int j=2; j<=ceil(sqrt(a)); j++){
+				if(a%j==0){
+					return 0;
+				}
+				return 1;
+			}
+		}	
+	}else{
+		int count=0;
+		for(int i=a; i<=b; i++){
+			int test=0;
+			for(int j=2; j<=ceil(sqrt(i)); j++){
+				if(i%j==0){
+					test++;
+					break;
+				}
+			}
+			if(test==0) count++;
+		}
+		return count;
+	}
 }
 
 int main(){
-	int index=1;
+	
 	int a,b;
-	count[1]=1;
-	for(long long i=2; i<Largest; i++){
-		if((fact(i-1)+1)/i%1==0) index++;
-		count[i] = index;
-	}
+	
 	while(cin>>a>>b){
-		cout << count[b]-count[a] << endl;
+		cout << check(a,b) << endl;
 	}
-} 
+}
