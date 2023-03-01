2579c0dc738640eabaa58e1424187628de7737ec
Xiang990293 <60068728+Xiang990293@users.noreply.github.com>
2023-03-01 21:22:57 +0800

    temp

--- a215_ming_ming_likes_counting.cpp
+++ a215_ming_ming_likes_counting.cpp
@@ -2,10 +2,24 @@
 #include<math.h>
 using namespace std;
 
-int main(){
-	int n, m;
-	while(cin>>n>>m){
-		int x=(-1+sqrt(1+4*(n*n-n+2*m)))/2;
-		cout << ceil(x) - n + 1 + floor(1-ceil(x)+(x)) << endl;
+long long check(int n, int m){
+	if(n>m) return 1;
+	if(n==m) return 2;
+	if(n*m<0){
+		if(m>0) return 2*abs(n)+1+check(abs(n)+1,m);
+		if(m==0) return 2*abs(n)+1;
+		if(m<0) return 2*abs(n)+1-check(1,n)+check(1,m);
+	}else{
+		long long x=(-1+sqrt(1+4*(n*n-n+2*m)))/2;
+		return ceil(x) - n + 1 + floor(1-ceil(x)+(x));
 	}
+}
+
+int main(){
+	long long n, m;
+	for(n=-30000; n<=30000; n++) for(m=n-1000; m<=n+1000; m++) if(check(n, m) < 0) cout << n << " " << m << endl;
+
+	// while(cin>>n>>m){
+	// 	cout << check(n,m) << endl;
+	// }
 }
