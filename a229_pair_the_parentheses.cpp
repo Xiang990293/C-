6d32e0f2f45494cf91b5366a6b0ea69ac4df10f1
Xiang990293 <60068728+Xiang990293@users.noreply.github.com>
2023-03-29 15:41:40 +0800

    update from temporary computer

--- a229_pair_the_parentheses.cpp
+++ a229_pair_the_parentheses.cpp
@@ -1,20 +1,40 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-string pairchanger(string a){
-	//making binpos, position with binary storing
-	unsigned int binpos = 0b0, btemp=0b0;
-	for(int i=a.length(); i>0; i--){
-		binpos <<= 1;
-		binpos += (a[i]=='('&& a[i+1]==')');
+unsigned int binposGen(string a, unsigned short start, unsigned short end){
+	unsigned int bp = 0b0;
+	for(int i=start; i<=end; i++){
+		bp <<= 1;
+		bp += (a[i]=='('&& a[i+1]==')');
 	}
 	
+	return bp;	
+}
+
+string pairchanger(string a, unsigned short start, unsigned short end, string endif){
+	//if input a is in final state given by parenthesespairer(),
+	//return directly to end all recrusion
+	if(a==endif) return a;
+	
+	//making binpos, position with binary storing
+	unsigned int binpos = binposGen(a,start,end);
+	
 	//find 1 in binpos
-	for(int i=a.length()-1; i>=0; i--){
-		btemp = binpos >> i;
-		cout << btemp << endl;
+	unsigned int btemp=0b0;
+	for(int i=end; i>=start; i--){
+		btemp = binpos >> i-1;
+		if(btemp&&(binpos!=0)){
+			int length = end-start+1;
+			//?
+			cout << a.replace(i,2,")(") << endl;
+			//?
+			pairchanger(a.replace(i,2,")(").substr(end-i), start, end, endif);
+		}
 		binpos -= btemp << i;
 	}
+	
+	return "";
 }
 
 void parenthesespairer(int n){
@@ -39,7 +59,12 @@ void parenthesespairer(int n){
 	}
 	a+=b;
 	
-	pairchanger(a); 
+	///set final state be like
+	string final="";
+	for(int i=0; i<n; i++) final+="()";
+	
+	///start recursion
+	pairchanger(a,1,2*(n-1)-1,final); 
 	
 	return;
 }
