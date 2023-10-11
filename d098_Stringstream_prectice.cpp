5ade2b7c9f8efe85787c1afa4addd164714662f5
Xiang990293 <990293kwi@gmail.com>
2023-10-11 11:21:23 +0800

    fixing d098 and let compiler work on ntou PC

--- d098_Stringstream_prectice.cpp
+++ d098_Stringstream_prectice.cpp
@@ -1,59 +1,36 @@
 #include<string>
 #include<sstream>
 #include<iostream>
-#include<cctype>
 #include<regex>
 using namespace std;
 
 int main(){
-	string str, copy, temp;
+	string str, copy, temp, num;
 	string replacement;
-	double sum;
-	int i=0;
+	int sum;
+	string stop;
 	while(getline(cin, str)){
-		i++;
 		sum = 0;
+		stop = "";
 		copy = str;
-		// if(i==35) cout << str << endl;
 		smatch match1;
-		while(regex_search(str, match1, regex ("(^| )\\b[0-9]+\\b($| )"))) {
-			cout << str << endl;
+		while(regex_search(str, match1, regex ("(^| )\\b(\\d+)\\b($| )"))) {
+			cout << stop << " " << str << endl;
+			if(stop == str) break;
+			stop = str;
+			num = match1[2].str();
 			temp = match1[0].str();
-			if(regex_match(temp, regex ("[^0-9]"))){
+			if(regex_match(num, regex ("[^0-9]"))){
+				str = regex_replace(str, regex ("\\b"+temp+"\\b"), " ", regex_constants::format_first_only);
 			}
 			try{
-				sum += stod(temp);
-				str = regex_replace(str, regex ("\\b"+temp+"\\b"), "", regex_constants::format_first_only);
+				sum += stoi(num);
+				str = regex_replace(str, regex ("\\b"+temp+"\\b"), " ", regex_constants::format_first_only);
 			}catch(exception){
-				str = regex_replace(str, regex ("\\b"+temp+"\\b"), "", regex_constants::format_first_only);
+				str = regex_replace(str, regex ("\\b"+temp+"\\b"), " ", regex_constants::format_first_only);
 				continue;
 			}
 		}
-		// cout << str << endl;
-
-		// regex_replace(copy, regex ("\\b[0-9]+\\b"), "");
-
-		// smatch match1;
-		// while(regex_search(copy, match1, regex ("\\b[0-9]+\\b"))) {
-		// 	cout << copy << endl;
-		// 	temp = match1[0].str();
-		// 	copy = regex_replace(copy, regex ("\\b"+temp+"\\b"), "");
-		// }
-
-		// cout << copy << endl;
-
-		// smatch match2;
-		// while(regex_search(copy, match2, regex ("\\b[^ ]+\\b"))) {
-		// 	temp = match2[0].str();
-		// 	str = regex_replace(str, regex (""+temp+""), "");
-		// }
-
-		// smatch match3;
-		// while(regex_search(copy, match3, regex ("\\(?<= |^\\)\\b\\(.+?\\)\\b\\(?= |$\\)"))) {
-		// 	temp = match3[0].str();
-		// 	sum += stoi(temp);
-		// 	str = regex_replace(str, regex (temp), "");
-		// }
 	
 		cout << sum << endl;
 	}
--- temp.cpp
+++ temp.cpp
@@ -6,36 +6,45 @@
 #include<sstream>
 using namespace std;
 
-vector<double> add(vector<double> v1, vector<double> v2) {
-	int size = v1.size();
-	double sum[size];
+// vector<double> add(vector<double> v1, vector<double> v2) {
+// 	int size = v1.size();
+// 	double sum[size];
 
-    for (int i = 0; i < size; i++) sum[i] = (v1[i] + v2[i]);
+//     for (int i = 0; i < size; i++) sum[i] = (v1[i] + v2[i]);
 
-	vector<double> s(sum, sum+size);
+// 	vector<double> s(sum, sum+size);
 
-    return s;
-}
+//     return s;
+// }
 
 int main(){
-	istringstream s;
-	// string str;
-	string replacement;
-	int sum;09
-	// s >> str;
-	// while(str!=""){
-		sum = 0;
-		smatch match1;
-		string str = "45 }45 1a6f 6 65afd 15s 1sa 12 115 11{";
+	// istringstream s;
+	string str;
+	// string replacement;
+	// int sum;
+	// // s >> str;
+	// // while(str!=""){
+	// 	cin >> sum;
+	// 	cout << sum+1 << endl;
+	// 	sum = 0;
+	smatch match1;
+	// 	string str = "45 }45 1a6f 6 65afd 15s 1sa 12 115 11{";
+	// 	// regex_search(str, match1, regex ("(^| )\\b([0-9]+)\\b($| )"));
+	// 	s >> str;
+	// // }
+
+	string temp;
+	while(getline(cin, str)) {
 		regex_search(str, match1, regex ("(^| )\\b([0-9]+)\\b($| )"));
-		cout << match1[0] << endl;
-		cout << match1[1] << endl;
-		cout << match1[2] << endl;
-		cout << match1[3] << endl;
-		cout << match1[4] << endl;
-		cout << match1[5] << endl;
-		s >> str;
-	// }
+		cout << "str: '" << str << "'" << endl;
+		temp = match1[0].str();
+		cout << "match1[0]: '" << match1[0] << "'" << endl;
+		cout << "match1[1]: '" << match1[1] << "'" << endl;
+		cout << "match1[2]: '" << match1[2] << "'" << endl;
+		cout << "match1[3]: '" << match1[3] << "'" << endl;
+		cout << "match1[4]: '" << match1[4] << "'" << endl;
+		cout << "match1[5]: '" << match1[5] << "'" << endl;
+	}
 
 	return 0;
 }
