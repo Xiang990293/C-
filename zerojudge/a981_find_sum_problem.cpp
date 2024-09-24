973c5480d27b899a78aa92dc3905b27738031579
Xiang990293 <60068728+Xiang990293@users.noreply.github.com>
2024-09-24 12:01:11 +0800

    ntou a981 think

---
+++ zerojudge/a981_find_sum_problem.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int parse_goal(int* arr, int goal)
+
+int main(){
+	int num_of_num;
+	int goal;
+	int nums[30];
+	while (cin >> num_of_num >> goal){
+		for(int _=0; _<num_of_num; _++){
+			cin >> nums[_];
+		}
+		
+		for(int i=0; i<num_of_num; i++)
+			for(int j=i+1; j<num_of_num; j++){
+			
+				
+		}
+	}
+	
+	return 0;
+}
