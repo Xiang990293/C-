c1addba97623d7420c889b27d1a50ce196d6749f
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-09-08 14:21:27 +0800

    using hash table to lookup prefix_counts instead

--- leetcode/560.Subarray_Sum_Equals_K.cpp
+++ leetcode/560.Subarray_Sum_Equals_K.cpp
@@ -5,14 +5,15 @@ public:
 
         int sum = 0;
         int counter = 0;
+        std::unordered_map<int, int> prefix_counts;
+        prefix_counts[0] = 1;
         for (int &i : nums) {
             sum += i;
 
-            for (int &j : prefix_sum) {
-                if (j == sum - k) counter++;
-            }
+            if (prefix_counts.find(sum - k) != prefix_counts.end()) counter+=prefix_counts[sum-k];
 
             prefix_sum.push_back(sum);
+            prefix_counts[sum]++;
         }
 
         return counter;
