a826c2deda12556b68ad8a0f3ba8d15d6f386629
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-09-14 12:52:24 +0900

    Implement food categorization program

---
+++ zerojudge/e839.P6.飲食分類_(Food).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    unordered_map<string, vector<string>> food_cat;
+
+    int cnt = 0;
+    cin >> cnt;
+    for (int i = 0; i < cnt; i++) {
+        string F = "";
+        string S = "";
+        cin >> F >> S;
+        food_cat[S].push_back(F);
+    }
+
+    string C = "";
+    cin >> C;
+    if (food_cat[C].empty()) cout << "No" << endl;
+    else {
+        sort(food_cat[C].begin(), food_cat[C].end(), [](string a, string b){
+            return a < b;
+        });
+        for (string &i : food_cat[C]) {
+            cout << i << endl;
+        }
+    }
+}
