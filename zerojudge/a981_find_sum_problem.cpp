b25c8191659bbd1bd8da29903d19742a10cb4176
翔越風 <60068728+Xiang990293@users.noreply.github.com>
2026-09-18 12:26:18 +0900

    attempt 1 zerojudge/a981

--- zerojudge/a981_find_sum_problem.cpp
+++ zerojudge/a981_find_sum_problem.cpp
@@ -1,58 +1,45 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<algorithm>
 using namespace std;
 
-int subarraySum(vector<int>& nums, int k) {
-	vector<int> prefix_sum = {0};
-
-	int sum = 0;
-	int counter = 0;
-	unordered_map<int, int> prefix_counts;
-	prefix_counts[0] = 1;
-	for (int &i : nums) {
-		sum += i;
-
-		if (prefix_counts.find(sum - k) != prefix_counts.end()) counter+=prefix_counts[sum-k];
-
-		prefix_sum.push_back(sum);
-		prefix_counts[sum]++;
+void helper(vector<int> &nums, int pos, int sum, string result, int target) {
+	if (sum > target) return;
+	if (sum == target) {
+		cout << result << endl;
+		return;
+	}
+	if (pos == nums.size()) {
+		return;
 	}
 
-	return counter;
+	helper(nums, pos+1, sum+nums[pos], result + to_string(nums[pos]) + " ", target);
+	helper(nums, pos+1, sum, result, target);
+
+	return;
 }
 
 int main(){
-	int num_of_num;
-	int goal;
-	int nums[30];
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	
-	
+	int n, tar;
+	cin >> n >> tar;
+	vector<int> nums(n, 0);
+
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
+
+	sort(nums.begin(), nums.end(), [](int a, int b) {
+		return a < b;
+	});
+
+	helper(nums, 0, 0, "", tar);
 	
 	return 0;
 }
 
 // similar to leetcode 560
 // BUT DIFFERENT!!!
-
-// class Solution {
-// public:
-//     int subarraySum(vector<int>& nums, int k) {
-//         vector<int> prefix_sum = {0};
-
-//         int sum = 0;
-//         int counter = 0;
-//         std::unordered_map<int, int> prefix_counts;
-//         prefix_counts[0] = 1;
-//         for (int &i : nums) {
-//             sum += i;
-
-//             if (prefix_counts.find(sum - k) != prefix_counts.end()) counter+=prefix_counts[sum-k];
-
-//             prefix_sum.push_back(sum);
-//             prefix_counts[sum]++;
-//         }
-
-//         return counter;
-//     }
-// };
